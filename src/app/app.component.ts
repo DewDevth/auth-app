b69342ecd03eea2ce8931a8d70bdacfa7d635fa6
DewDevth <140059595+DewDevth@users.noreply.github.com>
2023-07-24 10:50:41 +0700

    version later

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,31 @@
 import { Component, DoCheck } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements DoCheck {
   title = 'auth-app';
-  ismenurequired = false
-  constructor(private router: Router) {
+  ismenurequired = false;
+  isadminuser = false;
 
-  }
+  constructor(private router: Router, private service: AuthService) {}
 
   //แสดง navbar ตอน loginเเล้ว
   ngDoCheck(): void {
     let currenturl = this.router.url;
     if (currenturl == '/login' || currenturl == '/register') {
-      this.ismenurequired = false
+      this.ismenurequired = false;
     } else {
-      this.ismenurequired = true
+      this.ismenurequired = true;
+    }
+    if (this.service.GetUserrole() === 'admin') {
+      this.isadminuser = true;
+    } else {
+      this.isadminuser = false;
     }
   }
 }
--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,16 +1,43 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
 import { AuthService } from '../service/auth.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router, private toastr: ToastrService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
     // Check if the user is authenticated or meets certain conditions.
     if (this.authService.IsloggedIn()) {
-      return true; // Allow navigation to the requested route.
+      if (route.url.length > 0) {
+        let menu = route.url[0].path;
+        if (menu == 'user') {
+          if (this.authService.GetUserrole() == 'admin') {
+            return true;
+          } else {
+            this.toastr.warning('you dont have access');
+            this.router.navigate(['']);
+            return false;
+          }
+        } else {
+          return true;
+        }
+      } else {
+        return true; // Allow navigation to the requested route.
+      }
     } else {
       // Redirect the user to the login page or another appropriate route.
       this.router.navigate(['/login']);
